3be7e3501f9a4bb864f23736692fa4b0742ae2e8
Patrick Fish <patrickfish10@icloud.com>
2025-07-16 23:05:12 -0400

    refactor to include only client components in frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,38 @@
-import UserInput from '@/components/client/UserInput';
-import TimeZone from '@/components/server/TimeZone';
+'use client';
+import { useState } from 'react';
+import UserInput from '@/components/UserInput';
+import Loading from '@/components/Loading';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [weather, setWeather] = useState<any>(null);
+
+  const handleSearch = async (location: string) => {
+    setIsLoading(true);
+    try {
+      // const res = await fetch('');
+      // const data = await res.json();
+      // setWeather(data);
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      setWeather(" ");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
       <div className="flex flex-col items-center justify-center h-screen w-screen bg-[#777]">
-        <UserInput onLocationSubmit={(location: string) => {}}/>
+        { weather ? (
+          <div>Weather Info</div>
+        ) : (
+          isLoading ? (
+            <Loading />
+          ) : (
+            <UserInput onSearch={handleSearch}/>
+          )
+        )}
       </div>
   );
 }
