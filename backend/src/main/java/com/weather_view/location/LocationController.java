465fbab34f6aaa5b228794e73e77631fd245d212
Patrick Fish <patrickfish10@icloud.com>
2025-07-22 23:04:30 -0400

    refactored getLocation to be a static method

--- backend/src/main/java/com/weather_view/location/LocationController.java
+++ backend/src/main/java/com/weather_view/location/LocationController.java
@@ -1,31 +1,28 @@
 package com.weather_view.location;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
-
+    
 @RestController
 public class LocationController {
     
-    private final RestClient restClient;
+    private static RestClient restClient;
 
     public LocationController(RestClient.Builder restClientBuilder) {
-        this.restClient = restClientBuilder.baseUrl("").build();
+        restClient = restClientBuilder.baseUrl("").build();
     }
 
-    @GetMapping("/api/location")
-    public Location getLocation(  @RequestParam("city") String city,
-                                @RequestParam(value = "state", required = false) String state,
-                                @RequestParam("country") String country,
-                                @Value("${geolocater.key}") String locationKey
-                                ) {
+    public static Location getLocation(
+        String city,
+        String state,
+        String country,
+        String locationKey
+        ) {
         String query = (state != null && !state.trim().isEmpty())
                 ? String.format("%s,%s,%s", city, state, country)
                 : String.format("%s,%s", city, country);
         String uri = String.format("http://api.openweathermap.org/geo/1.0/direct?q=%s&appid=%s&limit=1", query, locationKey);
-        Location[] locations = this.restClient.get()
+        Location[] locations = restClient.get()
             .uri(uri)
             .retrieve()
             .body(Location[].class);
