f3863dfc0694eab675c6d553852473328c5d00f1
Patrick Fish <patrickfish10@icloud.com>
2025-07-29 23:59:24 -0400

    fetched api endpoint from frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,15 +7,29 @@ import WeatherOutput from '@/components/WeatherOutput';
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [weather, setWeather] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const handleSearch = async (city: string, state: string, country: string) => {
+    setIsError(false);
     setIsLoading(true);
     try {
       console.log(city, state, country);
-      await new Promise(resolve => setTimeout(resolve, 3000));
-      setWeather(true);
+      let response: any;
+      if (state) {
+        response = await fetch(`http://localhost:8080/api/entry?city=${city}&state=${state}&country=${country}`);
+      } else {
+        response = await fetch(`http://localhost:8080/api/entry?city=${city}&country=${country}`);
+      }
+
+      // checks response had no issue
+      if (response.ok) {
+        setWeather(true);
+      } else {
+        setIsError(true);
+      }
     } catch (error) {
       console.error(error);
+      setIsError(true);
     } finally {
       setIsLoading(false);
     }
@@ -29,7 +43,12 @@ export default function Home() {
           isLoading ? (
             <Loading />
           ) : (
-            <UserInput onSearch={handleSearch}/>
+            <>
+              <UserInput onSearch={handleSearch}/>
+              {isError && (
+                <h1>Error Occurred</h1>
+              )}
+            </>
           )
         )}
       </div>
