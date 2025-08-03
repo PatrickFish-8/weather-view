a308f9818946801a4f013cb99cffe115c00dbf86
Patrick Fish <patrickfish10@icloud.com>
2025-08-02 23:38:13 -0400

    styling for WeatherOutput and reset button

--- frontend/src/components/WeatherOutput.tsx
+++ frontend/src/components/WeatherOutput.tsx
@@ -1,7 +1,27 @@
-export default function WeatherOutput() {
+import { Button } from '@mantine/core';
+
+export default function WeatherOutput(
+    { onReset } : { onReset: () => void }) {
     return (
-        <div>
-            <img src="./generated_image.png"/>
+        <div className="flex flex-col items-center gap-5 justify-center h-[100%] m-0 p-0">
+            <div className="h-3/4 p-[1%] flex justify-center bg-[#4e5e53]/50">
+                <img className="aspect-square" src="./generated_image.png"/>
+            </div>
+            <Button
+                styles={{
+                    root: {
+                        backgroundColor: '#ff7034',
+                        fontSize: "25px",
+                        width: "75%",
+                        borderColor: "#f2f0ef",
+                        color: "#f2f0ef",
+                        borderWidth: "2px",
+                    },
+                }}
+                onClick={() => onReset()}
+            >
+                reset
+            </Button>
         </div>
     )
 }
